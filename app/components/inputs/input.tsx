a3919b1afeb57547fe6f85da5dc7b69b38f51a32
JulioDev1 <juliocesargualberto617@gmail.com>
2025-04-16 13:56:27 -0300

    feautre-delete: delete products

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -6,7 +6,8 @@ export default function Input(props:input){
             className="border rounded-md h-10 pl-2 border-gray-300" 
             type={props.type}
             name={props.name} 
-            placeholder={props.placeholder} />
+            placeholder={props.placeholder} 
+        />
     )
 
 }
--- app/services/products.ts
+++ app/services/products.ts
@@ -2,7 +2,7 @@
 import { ProductResponse } from "../management-products/Dto/ProductResponse"
 import { url } from "./url"
 
-export default async function GetAdminProducts(token:string, page:number):Promise<ProductResponse>{
+export async function GetAdminProducts(token:string, page:number):Promise<ProductResponse>{
 
     const Headers: HeadersInit = {
         "Access-Control-Allow-Origin": "*",
@@ -19,4 +19,17 @@ export default async function GetAdminProducts(token:string, page:number):Promis
     const data = await response.json() as ProductResponse
     console.log(data)
     return data;
+}
+export async function DeleteProduct(token:string, id:number):Promise<void>{
+    const Headers: HeadersInit = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Content-Type": "application/json",
+        "Authorization":`Bearer ${token}`
+    }
+
+    await fetch(`${url}/api/Product/delete-product-by-id?productId=${id}`, {
+        method: 'DELETE',
+        headers: Headers
+    })
 }
